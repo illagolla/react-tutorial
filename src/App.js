61d6e7081e066ea4cebb86dd850af098937eb7c4
illagolla <108179002+illagolla@users.noreply.github.com>
2023-01-09 19:15:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import Final from "./tutorial/1-useState/final/5-useState-counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Setup />
+      {/* <Final /> */}
     </div>
   );
 }
---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const UseStateBasics = () => {
+  // console.log(useState());
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button type="button" onClick={handleClick} className="btn">
+        change title
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default UseStateBasics;
