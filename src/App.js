179c6334247906442f5ab0c1fa4c1dabab95df0e
illagolla <108179002+illagolla@users.noreply.github.com>
2023-01-10 14:21:26 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-import Final from "./tutorial/1-useState/final/5-useState-counter";
+import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+// import Final from "./tutorial/3-conditional-rendering/final/1-multiple-returns";
 
 function App() {
   return (
